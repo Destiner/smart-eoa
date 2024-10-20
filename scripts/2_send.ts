8409cfff307e32dd30265faad4973a199ef3455b
Timur Badretdinov <accounts@destiner.io>
2024-10-20 13:33:23 +0100

    Use bundler

--- scripts/2_send.ts
+++ scripts/2_send.ts
@@ -7,10 +7,15 @@ import {
   Hex,
   http,
   keccak256,
+  padHex,
 } from "viem";
 import { odysseyTestnet } from "viem/chains";
 import { privateKeyToAccount, sign } from "viem/accounts";
-import { readContract, writeContract } from "viem/actions";
+import { readContract } from "viem/actions";
+import {
+  createBundlerClient,
+  sendUserOperation,
+} from "viem/account-abstraction";
 
 import entryPoint070Abi from "@/abi/entryPoint0_7_0.js";
 import kernelV3ImplementationAbi from "@/abi/kernelV3Implementation.js";
@@ -29,21 +34,32 @@ interface Op_0_7 {
 
 const ENTRY_POINT_0_7_0 = "0x0000000071727de22e5e9d8baf0edac6f37da032";
 
+// Fix: make it dynamic
+const callGasLimit = 327680n;
+const verificationGasLimit = 1048576n;
+const preVerificationGas = 100000n;
+const maxPriorityFeePerGas = 1500000000n;
+const maxFeePerGas = 2000000000n;
+
 const ownerPrivateKey = process.env.OWNER_PRIVATE_KEY as Hex | undefined;
 if (!ownerPrivateKey) {
   throw new Error("OWNER_PRIVATE_KEY is required");
 }
 const owner = privateKeyToAccount(ownerPrivateKey);
+console.log("Owner Address:", owner.address);
 
-const sponsorPrivateKey = process.env.SPONSOR_PRIVATE_KEY as Hex | undefined;
-if (!sponsorPrivateKey) {
-  throw new Error("SPONSOR_PRIVATE_KEY is required");
+const bundlerRpc = process.env.BUNDLER_RPC;
+if (!bundlerRpc) {
+  throw new Error("BUNDLER_RPC is required");
 }
-const sponsorAccount = privateKeyToAccount(sponsorPrivateKey);
+
 const client = createClient({
-  account: sponsorAccount,
   chain: odysseyTestnet,
   transport: http(),
+});
+const bundlerClient = createBundlerClient({
+  client,
+  transport: http(bundlerRpc),
 });
 
 const execMode =
@@ -90,6 +106,59 @@ const callData = encodeFunctionData({
   args: [execMode, executionCalldata],
 });
 
+const nonce = await readContract(client, {
+  address: ENTRY_POINT_0_7_0,
+  abi: entryPoint070Abi,
+  functionName: "getNonce",
+  args: [owner.address, 0n],
+});
+
+const op: Op_0_7 = {
+  sender: owner.address,
+  nonce,
+  // Should be already initialized
+  initCode: "0x",
+  callData,
+  accountGasLimits: concat([
+    padHex(verificationGasLimit.toString(16) as Hex, { size: 16 }),
+    padHex(callGasLimit.toString(16) as Hex, { size: 16 }),
+  ]),
+  preVerificationGas,
+  gasFees: concat([
+    padHex(maxPriorityFeePerGas.toString(16) as Hex, { size: 16 }),
+    padHex(maxFeePerGas.toString(16) as Hex, { size: 16 }),
+  ]),
+  paymasterAndData: "0x",
+  signature: "0x",
+};
+
+const opHash = getOpHash(odysseyTestnet.id, ENTRY_POINT_0_7_0, op);
+if (!opHash) {
+  console.error("Failed to get op hash");
+  process.exit(1);
+}
+const signature = await sign({
+  hash: opHash,
+  privateKey: ownerPrivateKey,
+  to: "hex",
+});
+console.log("Signature:", signature);
+
+const userOpHash = await sendUserOperation(bundlerClient, {
+  entryPointAddress: ENTRY_POINT_0_7_0,
+  sender: owner.address,
+  nonce,
+  callData,
+  callGasLimit,
+  verificationGasLimit,
+  preVerificationGas,
+  maxPriorityFeePerGas,
+  maxFeePerGas,
+  signature,
+});
+
+console.log("Op hash:", userOpHash);
+
 function getOpHash(chain: number, entryPoint: Address, op: Op_0_7): Hex | null {
   const hashedInitCode = keccak256(op.initCode);
   const hashedCallData = keccak256(op.callData);
@@ -122,61 +191,3 @@ function getOpHash(chain: number, entryPoint: Address, op: Op_0_7): Hex | null {
   );
   return keccak256(encoded);
 }
-
-const nonce = await readContract(client, {
-  address: ENTRY_POINT_0_7_0,
-  abi: entryPoint070Abi,
-  functionName: "getNonce",
-  args: [owner.address, 0n],
-});
-
-const op: Op_0_7 = {
-  sender: owner.address,
-  nonce,
-  // Should be already initialized
-  initCode: "0x",
-  callData,
-  // Fix: make it dynamic
-  accountGasLimits:
-    "0x00000000000000000000000000030ecd00000000000000000000000000064fc0",
-  // Fix: make it dynamic
-  preVerificationGas: 100000n,
-  // Fix: make it dynamic
-  gasFees: "0x0000000000000000000000000010eff00000000000000000000000000033915f",
-  paymasterAndData: "0x",
-  signature: "0x",
-};
-
-const opHash = getOpHash(odysseyTestnet.id, ENTRY_POINT_0_7_0, op);
-if (!opHash) {
-  console.error("Failed to get op hash");
-  process.exit(1);
-}
-const signature = await sign({
-  hash: opHash,
-  privateKey: ownerPrivateKey,
-});
-console.log("Signature:", signature);
-
-const signatureHex = concat([
-  signature.r,
-  signature.s,
-  `0x${(signature.v || 0n).toString(16)}`,
-]);
-
-const txHash = await writeContract(client, {
-  address: ENTRY_POINT_0_7_0,
-  abi: entryPoint070Abi,
-  functionName: "handleOps",
-  args: [
-    [
-      {
-        ...op,
-        signature: signatureHex,
-      },
-    ],
-    sponsorAccount.address,
-  ],
-});
-
-console.log("Tx hash:", txHash);
